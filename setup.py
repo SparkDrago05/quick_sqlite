e7a4062b74f029f3e0576f8874f7c61fbeb74961
SparkDrago05 <69835321+SparkDrago05@users.noreply.github.com>
2022-09-02 20:11:13 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
     ],
     
 )
